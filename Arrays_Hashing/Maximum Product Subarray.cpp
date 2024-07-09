9430af3a7db6e0905e37e7161f8facc9821345be
Sujan Ghosh <103595490+sujanrupu@users.noreply.github.com>
2024-07-09 16:48:39 +0530

    Create Maximum Product Subarray.cpp

---
+++ Arrays_Hashing/Maximum Product Subarray.cpp
@@ -0,0 +1,13 @@
+class Solution {
+public:
+    int maxProduct(vector<int>& nums) {
+        int minProd = nums[0], maxProd = nums[0], ans = nums[0];
+        for(int i = 1; i < nums.size(); i++) {
+            int temp = minProd;
+            minProd = min({nums[i], minProd * nums[i], maxProd * nums[i]});
+            maxProd = max({nums[i], temp * nums[i], maxProd * nums[i]});
+            ans = max(ans, maxProd);
+        }
+        return ans;
+    }
+};
