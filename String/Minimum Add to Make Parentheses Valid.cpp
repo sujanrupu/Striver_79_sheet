ffb5f47735e963d1513c8de03c78309919dde6d0
Sujan Ghosh <103595490+sujanrupu@users.noreply.github.com>
2024-07-10 09:47:05 +0530

    Create Minimum Add to Make Parentheses Valid.cpp

---
+++ String/Minimum Add to Make Parentheses Valid.cpp
@@ -0,0 +1,12 @@
+class Solution {
+public:
+    int minAddToMakeValid(string s) {
+        stack<char>st;
+        for(int i = 0; i < s.length(); i++) {
+            if(st.empty()) st.push(s[i]);
+            else if(st.top() == '(' && s[i] == ')') st.pop();
+            else    st.push(s[i]);
+        }
+        return st.size();
+    }
+};
